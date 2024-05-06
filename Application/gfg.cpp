de6d0826e11e06437e2558e94e78a537a045e8b8
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-06 13:41:28 +0530

    06/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -441,3 +441,36 @@ class Solution{
         return ans;
     }
 };
+// ***********************************************************06/05
+vector<int> noSibling(Node* node)
+{
+   queue<Node*> q;
+   vector<int> ans;
+   if(node == NULL){
+       return ans;
+    }
+   q.push(node);
+   while(q.empty() == false){
+       Node* curr = q.front();
+       q.pop();
+       if(curr->left != NULL){
+           if(curr->right == NULL){
+               ans.push_back(curr->left->data);
+           }
+           q.push(curr->left);
+       }
+       if(curr->right != NULL){
+           if(curr->left == NULL){
+               ans.push_back(curr->right->data);
+           }
+           q.push(curr->right);
+       }
+   }
+   if(ans.size() == 0){
+       return {-1};
+   }
+   else{
+       sort(ans.begin(), ans.end());
+       return ans;
+   }
+}
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -215,3 +215,36 @@ public:
         node -> next = node -> next -> next;
     }
 };
+// ********************************************************************06/05(Print All Node that don't have siblings)
+vector<int> noSibling(Node* node)
+{
+   queue<Node*> q;
+   vector<int> ans;
+   if(node == NULL){
+       return ans;
+    }   
+   q.push(node);
+   while(q.empty() == false){
+       Node* curr = q.front();
+       q.pop();
+       if(curr->left != NULL){
+           if(curr->right == NULL){
+               ans.push_back(curr->left->data);
+           }
+           q.push(curr->left);
+       }
+       if(curr->right != NULL){
+           if(curr->left == NULL){
+               ans.push_back(curr->right->data);
+           }
+           q.push(curr->right);
+       }
+   }
+   if(ans.size() == 0){
+       return {-1};
+   }
+   else{
+       sort(ans.begin(), ans.end());
+       return ans;
+   }
+}
