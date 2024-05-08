4c8de7b922cfb85a6332953194c497fde958df6b
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-08 20:07:12 +0530

    08/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -498,3 +498,31 @@ vector<int> reverseLevelOrder(Node *root)
     reverse(ans.begin(),ans.end());
     return ans;
 }
+// ************************************************************08/05
+class Solution {
+     private:
+    void findingPaths(Node *root, vector<vector<int>> &paths, vector<int> &path){
+        if(root == NULL)
+            return;
+        
+        path.push_back(root->data);
+        
+        if(root->left == NULL && root->right == NULL)
+            paths.push_back(path);
+            
+        findingPaths(root->left, paths, path);
+        findingPaths(root->right, paths, path);
+        
+        path.pop_back();
+    }
+  public:
+    vector<vector<int>> Paths(Node* root) {
+        // code here
+    vector<vector<int>> paths;
+        vector<int> path;
+        
+        findingPaths(root, paths, path);
+        
+        return paths;
+    }
+};
