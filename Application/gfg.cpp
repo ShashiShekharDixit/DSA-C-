8042da29afaf682c1586597285d899e228d7f965
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-10 19:48:08 +0530

    10/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -551,3 +551,35 @@ int main() {
     }
     return 0;
 }
+// *****************************************************************10/05
+class Solution{
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>& arr, int index, vector<int>& path, long long sum, int k)
+    {
+        if(sum >= k){
+            if(sum == k)
+            ans.push_back(path);
+            return;
+        }
+        if(index >= arr.size())
+        {
+          return;            
+        }
+        path.push_back(arr[index]);
+        solve(arr, index + 1, path, sum + arr[index], k);
+        path.pop_back();
+        while(index + 1 < arr.size() && arr[index] == arr[index + 1])
+        index++;
+        solve(arr, index + 1, path, sum, k);
+    }
+    vector<vector<int>> CombinationSum2(vector<int> arr,int n,int k)
+    {
+        //code here
+        sort(arr.begin(), arr.end());
+        vector<int> path;
+        long long sum = 0;
+        solve(arr, 0, path, sum, k);
+        return ans;
+    }
+};
