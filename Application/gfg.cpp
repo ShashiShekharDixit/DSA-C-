c704e592f8c35027c83757129b82beb38b5b5fa8
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-11 18:16:21 +0530

    11/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -583,3 +583,30 @@ public:
         return ans;
     }
 };
+// *******************************************************************11/05(Juggler Sequence)
+class Solution {
+  public:
+    vector<long long> ans;
+    map<long long, long long>mp;
+    int recur(long long x){
+        if(x <= 1) return 0;
+        if(mp[x] != 0) return mp[x];
+        mp[x] = 1;
+        if(x % 2 == 0){
+            x = sqrt(x);
+            ans.push_back(x);
+            return recur(x);
+        }
+        if(x % 2 == 1){
+            x = x * sqrt(x);
+            ans.push_back(x);
+            return recur(x);
+        }
+    }
+    vector<long long> jugglerSequence(long long n) {
+        // code here
+        ans.push_back(n);
+        int dummy = recur(n);
+        return ans;
+    }
+};
