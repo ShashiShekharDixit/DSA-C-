fccbbe5e6edc3c46fe55c96052b3368c87a62cc1
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-16 17:49:35 +0530

    16/05

--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -534,3 +534,20 @@ public:
         return -1;
     }
 };
+// ********************************************************************16/05(Evaluate Boolean Binary Tree)
+class Solution {
+public:
+    bool helper(TreeNode* root) {
+        if(root -> val == 0 || root -> val == 1) {
+            return root -> val == 1;
+        } else if(root -> val == 2) {
+            return helper(root -> left) || helper(root -> right);
+        } else if(root -> val == 3) {
+            return helper(root -> left) && helper(root -> right);
+        } 
+        return false;
+    }
+    bool evaluateTree(TreeNode* root) {
+        return helper(root);
+    }
+};
