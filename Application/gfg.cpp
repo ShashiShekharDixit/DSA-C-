64dfbaa34eb5fc1bcc64b05a3d9c18311e3e9bfc
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-17 18:55:51 +0530

    17/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -808,3 +808,22 @@ public:
         return components;
     }
 };
+// ****************************************************************17/05(Find Pair Given Difference)
+class Solution {
+public:
+    int findPair(int n, int x, vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+            for (int i = 0; i < n; ++i) {
+            int find = arr[i] - x;
+            if (binary_search(arr.begin(), arr.end(), find)) {
+                if (find == arr[i] && x == 0) {
+                    int lb = lower_bound(arr.begin(), arr.end(), find) - arr.begin();
+                    if (lb != i) return 1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -551,3 +551,15 @@ public:
         return helper(root);
     }
 };
+// *********************************************************************17/05(Delete Leaves with a given value)
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) return nullptr;
+        root -> left = removeLeafNodes(root -> left, target);
+        root -> right = removeLeafNodes(root -> right, target);
+        if(!root -> left && !root -> right && root -> val == target)
+        return nullptr;
+        return root;
+    }
+};
