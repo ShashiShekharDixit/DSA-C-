0192f2eeea090f24c7eea77f4561ea871d48ae06
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-27 07:40:03 +0530

    27/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1036,3 +1036,19 @@ class Solution {
         return prev[m];
     }
 };
+// ***********************************************************************Longest Subsequence I(27/05)
+class Solution {
+  public:
+    int longestSubseq(int n, vector<int> &a) {
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        function<int(int, int)> rec = [&](int i, int p) -> int {
+            if(i == n) return 0;
+            if(p != -1 && dp[i][p] != -1) return dp[i][p];
+            int tk = 0, nt = rec(i + 1, p);
+            if(p == -1 || abs(a[i] - a[p]) == 1) tk = 1 + rec(i + 1, i);
+            if(p == -1) return max(tk, nt);
+            return dp[i][p] = max(tk, nt);
+        };
+        return rec(0, -1);
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -789,3 +789,34 @@ auto init = []() {
     cout.tie(0);
     return 'c';
 }();
+// ****************************************************************27/05(Special Array with X Elements Greater than equal to X)
+class Solution {
+public:
+    int n;
+    inline int f(int x, vector<int>& nums){
+        int i=lower_bound(nums.begin(), nums.end(), x)-nums.begin();
+        return n-i;
+    }
+    inline int g(int x, vector<int>& nums){
+        return f(x, nums)-x;
+    }
+    int specialArray(vector<int>& nums) {
+        n=nums.size();
+        sort(nums.begin(), nums.end());
+        int l=0, r=n, m;
+        while (l <= r) {
+            m = (l+r) / 2;
+            int gm = g(m, nums);
+            if (gm == 0) return m;
+            else if (gm > 0) l = m+1;
+            else r = m-1;
+        }
+        return -1;  
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
