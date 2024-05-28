8bb37af8feb511b272126058a7d2151ff4fecb1f
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-28 10:26:54 +0530

    28/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1052,3 +1052,23 @@ class Solution {
         return rec(0, -1);
     }
 };
+// ***********************************************************************Minimum cost to fill given weight in a bag(28/05)
+class Solution {
+  public:
+  int help(int n, int w, vector<int> &cost, vector<int>&dp){
+      if(w == 0) return 0;
+      if(dp[w]!=-1) return dp[w];
+      int ans = INT_MAX;
+      for(int i = 1; i <= min(n, w); i++)
+      {
+          ans = min(ans, help(n, w - i, cost, dp)+cost[i-1]);
+      }
+      return dp[w] = ans;
+  }
+    int minimumCost(int n, int w, vector<int> &cost) {
+        vector<int>dp(w+1, -1);
+        int ans = help(n,w,cost,dp);
+        if(ans==INT_MAX) return -1;
+        return ans;
+    }
+};
