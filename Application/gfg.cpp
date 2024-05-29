679ad33a10a4a7159c6142b98d65dd13bdd4bcf4
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-05-29 08:58:40 +0530

    29/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1072,3 +1072,25 @@ class Solution {
         return ans;
     }
 };
+// ********************************************************************Geek ans its Game of Coins(29/05)
+class Solution {
+  public:
+    int solve(int n, int x, int y, vector<int> &dp)
+    {
+        if(n <= 1)
+        return n;
+        if(dp[n] != -1)
+        return dp[n];
+        int ans = 1;
+        if(n >= x)
+        ans &= solve(n - x, x, y, dp);
+        if(n >= y)
+        ans &= solve(n - y, x, y, dp);
+        ans &= solve(n - 1, x, y, dp);
+        return dp[n] = ans ^ 1;
+    }
+    int findWinner(int n, int x, int y) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, x, y, dp);
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -839,3 +839,27 @@ public:
         return max_length;
     }
 };
+// *********************************************************************29/05(Number of Steps to Reduce a Number in Binary Representation to One)
+class Solution {
+public:
+    int numSteps(string& s) {
+        int n=s.size(), div=n-1, add1=0, carry=0, cnt1=0;
+        for(int i=n-1; i>0; i--){
+            while(i>0 && (s[i]-'0'+carry)&1){
+                carry|=s[i]=='1';
+                cnt1++;
+                i--;
+            }
+            add1+=cnt1;
+            cnt1=0;
+            carry|=s[i]=='1';
+        }
+        return div+add1+carry;
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
