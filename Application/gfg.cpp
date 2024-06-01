0d6034f4b8a298eb2f165eeef37cbcc84bbb7a91
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-01 08:01:56 +0530

    01/05

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1133,3 +1133,22 @@ class Solution {
         return a + b;
     }
 };
+// *********************************************************************Odd Even Problem(01/05)
+class Solution {
+  public:
+    string oddEven(string s) {
+        vector<int> freq(26, 0);
+        for(auto &c: s)
+        freq[c-'a']++;
+        int ans = 0;
+        for(int i = 1; i <= 26; i++)
+        {
+            if(freq[i - 1] == 0)
+            continue;
+            if((i%2) ^ (freq[i - 1]% 2) == 0)
+            ans++;
+        }
+        if(ans % 2 != 0) return "ODD";
+        return "EVEN";
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -910,3 +910,20 @@ auto init = []() {
     cout.tie(0);
     return 'c';
 }();
+// *******************************************************************01/06(Score of a String)
+class Solution {
+public:
+    int scoreOfString(string& s) {
+        int n=s.size();
+        int ans=0;
+        for(int i=1; i<n; i++)
+            ans+=abs(s[i]-s[i-1]);
+        return ans;        
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
