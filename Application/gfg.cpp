82bb159ce5653f666c1c54e3b4921c3e3ba1ff87
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-03 18:53:57 +0530

    03/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1193,3 +1193,25 @@ class Solution {
         return ans;
     }
 };
+// ***************************************************************************trail of ones(03/06)
+class Solution {
+  public:
+    int numberOfConsecutiveOnes(int n) {
+        if( n == 1 || n == 0)
+        return 0;
+        if(n == 2)
+        return 1;
+        int mod = 1e9+7;
+        int sum = 1;
+        int t1 = 0;
+        int t2 = 1;
+        for(int i = 3; i <= n; i++)
+        {
+            int t = t1 + t2;
+            t1 = t2;
+            t2 = t % mod;
+            sum = ((sum * 2 % mod) + t%mod) % mod;
+        }
+        return sum % mod;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -939,3 +939,20 @@ public:
         }
     }
 };
+// **************************************************************************03/06(Append Character to string to make subsequence)
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int s_index = 0, t_index = 0;
+        int s_length = s.length(), t_length = t.length();
+    
+        while (s_index < s_length && t_index < t_length) {
+            if (s[s_index] == t[t_index]) {
+                t_index++;
+            }
+            s_index++;
+        }
+    
+        return t_length - t_index;
+    }
+};
