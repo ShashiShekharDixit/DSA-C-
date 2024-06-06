70e6ae50de4da2b6eb5ca82bb38d2f94700eeaf5
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-06 20:28:23 +0530

    06/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1273,3 +1273,28 @@ class Solution {
         return -1;        
     }
 };
+// *****************************************************************************Max Sum in the configuration(06/06)
+class solution {
+public:
+  long long max_sum(int a[], int n) {
+            long long s1 = 0;
+            long long ts = 0;
+            
+            for (int i = 0; i < n; i++) {
+                s1 += (long long)i * a[i];  
+                ts += (long long)a[i];      
+            }
+            
+            long long maxi = s1;
+            
+            
+            for (int i = n - 1; i >= 1; i--) {
+                s1 = s1 + ts - (long long)n * a[i];  
+                if (maxi < s1) {
+                    maxi = s1;
+                }
+            }
+            
+            return maxi;
+        }
+}
