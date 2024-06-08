35c92a88afcc859eb6e9fee44a153835a9b2743c
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-08 08:29:37 +0530

    08/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1313,3 +1313,19 @@ class Solution {
         return max_element(v.begin(), v.end()) - v.begin();
     }
 };
+// *****************************************************************************Index of an extra element(08/06)
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        int left = 0, right = n - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(mid < n - 1 && arr1[mid] == arr2[mid]){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
