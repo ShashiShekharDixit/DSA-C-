28156b0631c4cb9d7ffeeb08c345e8aef6efb378
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-09 11:27:08 +0530

    09/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1329,3 +1329,21 @@ class Solution {
         return left;
     }
 };
+// **************************************************************************Convert Array into Zig Zag fashion(09/06)
+class Solution {
+  public:
+    void zigZag(int n, vector<int> &arr) {
+        for(int i = 0; i < n - 1; i++){
+            if(i & 1){
+                if(arr[i] < arr[ i + 1]){
+                    swap(arr[i], arr[ i + 1]);
+                }
+            }
+            else{
+                if(arr[i] > arr[ i + 1]){
+                    swap(arr[i], arr[i + 1]);
+                }
+            }
+        }
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -1137,3 +1137,27 @@ public:
         return false;
     }
 };
+// **************************************************************************09/06(Subarrays Sum divisible by K)
+class Solution {
+public:
+    static int subarraysDivByK(vector<int>& nums, int k) {
+        vector<int> mod_k(k, 0);
+        int prefix=0;
+        mod_k[0]=1;
+
+        for(int x: nums){
+            prefix=(prefix+x)%k;
+            prefix+=((prefix<0)?k:0);
+            mod_k[prefix]++;
+        }
+        return accumulate(mod_k.begin(), mod_k.end(), 0, [](int sum, int x){
+            return sum+x*(x-1)/2;
+        });
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
