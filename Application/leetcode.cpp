e2c88a9a7b9ef45347f3a1f186cb68c4ffe79f72
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-17 12:11:30 +0530

    17/06

--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -1288,3 +1288,22 @@ public:
         return result;
     }
 };
+// *******************************************************************************17/06(Sum of Square Number)
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int divisor = 2; divisor * divisor <= c; divisor++){
+            if( c % divisor == 0){
+                int exponentCount = 0;
+                while(c % divisor == 0){
+                    exponentCount++;
+                    c /= divisor;
+                }
+                if(divisor % 4 == 3 && exponentCount % 2 != 0){
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+};
