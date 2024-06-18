a631a9ea8ce37d2fbba27c4e9f2b67c4eb86602c
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-18 08:44:59 +0530

    18/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1522,3 +1522,18 @@ class Solution {
         return "true";
     }
 };
+// ****************************************************************************Number of Rectangle in a Circle(18/06)
+class Solution {
+  public:
+    int rectanglesInCircle(int r) {
+        int count = 0;
+        for(int i = 1; i < 2 * r; i++){
+            for(int j = 1; j < 2 * r; j++){
+                if(i*i+j*j <= 4*r*r){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -1307,3 +1307,26 @@ public:
         return c % 4 != 3;
     }
 };
+// ***********************************************************************************18/06(Most Profit Assigning work)
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int maxDifficulty = *max_element(difficulty.begin(), difficulty.end());
+        vector<int> maxProfitUpToDifficulty(maxDifficulty + 1, 0);
+        for (int i = 0; i < difficulty.size(); ++i) {
+            maxProfitUpToDifficulty[difficulty[i]] = max(maxProfitUpToDifficulty[difficulty[i]], profit[i]);
+        }
+        for (int i = 1; i <= maxDifficulty; ++i) {
+            maxProfitUpToDifficulty[i] = max(maxProfitUpToDifficulty[i], maxProfitUpToDifficulty[i - 1]);
+        }
+        int totalProfit = 0;
+        for (int ability : worker) {
+            if (ability > maxDifficulty) {
+                totalProfit += maxProfitUpToDifficulty[maxDifficulty];
+            } else {
+                totalProfit += maxProfitUpToDifficulty[ability];
+            }
+        }
+        return totalProfit;
+    }
+};
