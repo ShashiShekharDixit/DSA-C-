352091e6ece4fceafe67c06e83734119edb14f9a
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-22 08:33:18 +0530

    22/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1598,3 +1598,36 @@ class Solution {
        return ans;
     }
 };
+// *********************************************************************************Extract the NUmber of String(22/06)
+class Solution {
+  public:
+    long long ExtractNumber(string sentence) {
+        long long res = -1;
+        int i = 0;
+        string temp = "";
+        bool flag = 0;
+        for(int i = 0; i < sentence.length(); i++){
+            if(!isdigit(sentence[i])) {
+                temp = "";
+                continue;
+            }
+            while(isdigit(sentence[i])){
+                if(i >= sentence.length()) break;
+                if(sentence[i] == '9'){
+                    flag = 1;
+                }
+                temp += sentence[i];
+                i++;
+            }
+            if(temp == "" || flag == 1){
+                flag = 0;
+                temp = "";
+                continue;
+            }
+            long long num = stoll(temp);
+            res = max(res, num);
+            temp = "";
+        }
+        return res;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -1430,3 +1430,21 @@ public:
         return initialSatisfaction + maxExtraSatisfaction;   
     }
 };
+// ******************************************************************************************22/06(Count Number of Nice Subarray)
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> cnt(n + 1, 0);
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for(int v : nums){
+            t += v & 1;
+            if(t - k >= 0){
+                ans += cnt[t - k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+};
