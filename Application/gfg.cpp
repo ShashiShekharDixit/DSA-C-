6903b61ebd554d7d2b2de3ddfcec58a828bbfa80
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-29 10:56:47 +0530

    29/06

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1749,3 +1749,16 @@ string pattern(vector<vector<int>> &arr) {
         return "-1";
     }
 }
+// ***************************************************************************************Identical Linked List(29/06)
+bool areIdentical(struct Node *head1, struct Node *head2) {
+    while(head1 && head2)
+    {
+        if(head1 -> data != head2 -> data)
+        return false;
+        head1 = head1 -> next;
+        head2 = head2 -> next;
+    }
+    if(head1 || head2)
+    return false;
+    return true;
+}
