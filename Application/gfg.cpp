df7fa68ac503c4a05871218b95c448b8d2022bbb
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-08 19:18:47 +0530

    08/07

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -1930,3 +1930,38 @@ class Solution {
         return left.size() > right.size()?left:right;
     }
 };
+// *********************************************************************************************Search in Rotated Sorted Array(08/07)
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        int low = 0, high = arr.size() - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == key)
+            {
+                return mid;
+            }
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= key && key < arr[mid])
+                {
+                    high = mid - 1;
+                }
+                else 
+                {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if(arr[mid] < key && key <= arr[high])
+                {
+                    low = mid + 1;
+                }
+                else 
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
