204cf363aa8bacc37e64a69b32151ab77b4b55b8
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-12 20:44:43 +0530

    12/07

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2073,3 +2073,19 @@ class Solution {
         return ans;
     }
 };
+// ************************************************************************************Roof To Leaf Path Sum(12/07)
+class Solution {
+  public:
+ bool solve(Node* root, int target, int sum) {
+    if (!root) return false;
+    sum += root->data;
+    if (!root->left && !root->right) {
+        return sum == target;
+    }
+    return solve(root->left, target, sum) || solve(root->right, target, sum);
+}
+    bool hasPathSum(Node *root, int target) {
+        // Your code here
+       return solve(root , target ,0);
+    }
+};
