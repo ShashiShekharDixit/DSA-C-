43424154b17eb82072a011dbdf76ffdd7079d0d8
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-17 08:15:34 +0530

    17/07

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2195,3 +2195,28 @@ class Solution {
         return ans;
     }
 };
+// ***************************************************************************************Construct Binary tree from Parent Array(17/07)
+class Solution {
+  public:
+    Node *createTree(vector<int> parent) {
+        unordered_map<int,Node*> nodes;
+        for(int i = 0; i < parent.size(); i++){
+            nodes[i] = new Node(i);
+        }
+        int rootval = -1;
+        for(int i = 0; i < parent.size(); i++){
+            int Par = parent[i];
+            int Child = i;
+            if(Par == -1){
+                rootval = i;
+                continue;
+            }
+            if(nodes[Par] -> left == nullptr){
+                nodes[Par] -> left = nodes[Child];
+            } else {
+                nodes[Par] -> right = nodes[Child];
+            }
+        }
+        return nodes[rootval];
+    }
+};
