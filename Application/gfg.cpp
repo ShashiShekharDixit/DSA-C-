6b4ea83166c858af93908c3a99176a8766bb4c6c
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-18 10:47:14 +0530

    18/07

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2195,7 +2195,7 @@ class Solution {
         return ans;
     }
 };
-// ***************************************************************************************Construct Binary tree from Parent Array(17/07)
+// **************************************************************************************Construct Binary tree from Parent Array(17/07)
 class Solution {
   public:
     Node *createTree(vector<int> parent) {
@@ -2218,5 +2218,29 @@ class Solution {
             }
         }
         return nodes[rootval];
+    }
+};
+// ****************************************************************************************Longest Alernative Subsequence(18/07)
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
+        if(arr.size() == 1)
+        return 1;
+        if(arr.size() == 2)
+        {
+            if(arr[0] == arr[1])
+            return 1;
+            return 2;
+        }
+        int ans = 1;
+        int pre = arr[0];
+        for(int i = 1; i < arr.size()-1; i++)
+        {
+            if(arr[i] > arr[i + 1] && arr[i] > pre)
+            ans++, pre = arr[i];
+            else if(arr[i] < pre && arr[i] < arr[i + 1])
+            ans++, pre=arr[i];
+        }
+        return ans + 1;
     }
 };
