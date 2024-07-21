1fc3bf1c96ab2c7b4539cd6b71a8fe9d141b0ce1
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-21 16:04:02 +0530

    Maximum Product subset of an Array

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2301,15 +2301,41 @@ class Solution {
             
             if(root->left != NULL && root->right == NULL||root->left == NULL && root->right != NULL){
              root = root->left?root->left:root->right; 
-             
             }
-            
-           
         }
     }
     Node *RemoveHalfNodes(Node *root) {
         solve(root); 
         
         return root ; 
+    }
+};
+// **************************************************************************************Maximum product subset of an Array(21/07)
+class Solution {
+  public:
+    long long int findMaxProduct(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return arr[0];
+        long long mod = 1e9+7;
+        int flag = 0;
+        long long prod = 1, mini = INT_MIN;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                flag = 1;
+                continue;
+            }
+            if(arr[i] > 0){
+                prod = ((prod)%mod * arr[i])%mod;
+            }
+            else if(arr[i] < 0){
+                prod = ((prod) %mod * arr[i]) % mod;
+                mini = max((long long) arr[i], mini);
+            }
+        }
+        if(prod < 0){
+            prod = prod / mini;
+        }
+        if(prod == 1 && flag == 1) return 0;
+        return prod;
     }
 };
