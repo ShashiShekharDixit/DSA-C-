05a5e4a60848eeb4f7a656b0065707dc7c9fbc50
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-25 10:10:42 +0530

    25/07

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2399,4 +2399,21 @@ class Solution
         return isBST(root-> right);
     }
 };
-
+// **********************************************************************************************Array To BST(25/07)
+class Solution {
+  public:
+    Node* solve(vector<int> &nums, int s, int e){
+        if(s > e){
+            return NULL;
+        }
+        int mid = (s + e) / 2;
+        Node* curr = new Node(nums[mid]);
+        curr -> left = solve(nums,s, mid - 1);
+        curr -> right = solve(nums, mid + 1, e);
+        return curr;
+    }
+    Node* sortedArrayToBST(vector<int>& nums) {
+        int s = 0, e = nums.size() - 1;
+        return solve(nums, s, e);
+    }
+};
