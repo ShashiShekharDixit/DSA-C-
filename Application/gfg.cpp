fd6c65242a0c8bc00b9857f2a7ce71236a2c8e76
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-04 17:45:30 +0530

    04/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2610,3 +2610,25 @@ class Solution {
         return ans;
     }
 };
+// ****************************************************************************************N meeting in one Room(04/08)
+class Solution {
+  public:
+    int maxMeetings(int n, int start[], int end[]) {
+        int count=1;
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++){
+            v.push_back({end[i],start[i]});
+        }
+        sort(v.begin(),v.end());
+        int last=v[0].first;
+        for(int i=1;i<n;i++){
+            if(v[i].second>last){
+                count++;
+                last=v[i].first;
+            }
+        }
+       
+        
+    return count;
+    }
+};
