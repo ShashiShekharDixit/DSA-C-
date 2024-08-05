f072ee037ef0242b2a1a7fdcda9ea314f71fa949
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-05 15:43:27 +0530

    05/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2627,8 +2627,26 @@ class Solution {
                 last=v[i].first;
             }
         }
-       
-        
     return count;
+    }
+};
+// *******************************************************************************************Bottom View Of binary Tree(05/08)
+class Solution {
+public:
+  vector <int> bottomView(Node *root) {
+        map<int,int>mp;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* node = q.front().first;
+            int level = q.front().second;
+            q.pop();
+            mp[level]=node->data;
+            if(node->left)q.push({node->left,level-1});
+            if(node->right)q.push({node->right,level+1});
+        }
+        vector<int>ans;
+        for(auto it:mp)ans.push_back(it.second);
+        return ans;
     }
 };
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -2639,4 +2639,24 @@ public:
         }
         return ans;
     }
+};
+// ******************************************************************************Kth Distinct String in an Array(05/08)
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        int distinctCount = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (isDistinct(arr, i)) {
+                distinctCount++;
+                if (distinctCount == k) {
+                    return arr[i];
+                }
+            }
+        }
+        return "";
+    }
+private:
+    bool isDistinct(vector<string>& arr, int index) {
+        return count(arr.begin(), arr.end(), arr[index]) == 1;
+    }
 }; 
