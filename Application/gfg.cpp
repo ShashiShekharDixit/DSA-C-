33d28e2985b3a7f2bbeae67f780ecee0565b34ce
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-06 20:14:57 +0530

    06/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2650,3 +2650,40 @@ public:
         return ans;
     }
 };
+// *******************************************************************************Validate in IP Address(06/08)
+class Solution {
+  public:
+    int isValid(string str) {
+        int n = str.size();
+        int dot = 0;
+        string tmp = "";
+        for(int i = 0; i < n; i++)
+        {
+            if(str[i] == '.')
+            {
+                dot++;
+                if(tmp.empty() || (tmp[0] == '0' && tmp.size() > 1))
+                {
+                    return false;
+                }
+                int num = stoi(tmp);
+                if(num < 0 || num > 255)
+                {
+                   return false; 
+                }
+                tmp = "";
+            }
+             else {
+                 tmp += str[i];
+             }
+        }
+        if(dot != 3)
+        return false;
+        if(tmp.empty() || (tmp[0] == '0' && tmp.size() > 1))
+        return false;
+        int num = stoi(tmp);
+        if(num < 0 || num > 255)
+        return false;
+        return true;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -2660,3 +2660,19 @@ private:
         return count(arr.begin(), arr.end(), arr[index]) == 1;
     }
 }; 
+// ******************************************************************************Minimum Number of Pushes to Type Word II(06/08)
+class Solution {
+public:
+    static int minimumPushes(string& word) {
+        int freq[26]={0};
+        for(char c: word) 
+            freq[c-'a']++;
+        sort(freq, freq+26);
+    
+        int sz=25, ans=0;
+        for(; sz>=0 && freq[sz]!=0; sz--){
+            ans+=freq[sz]*((25-sz)/8+1);    
+        }
+        return ans;
+    }
+};
