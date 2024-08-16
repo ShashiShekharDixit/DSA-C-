8a26c4c76b3c867e2952472e6266441d25cd7496
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-16 18:49:10 +0530

    16/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -2899,3 +2899,33 @@ class Solution
         return reverse(head);
     }
 };
+// ************************************************************************************Maximize the Cut Segment(16/08)
+class Solution
+{
+    public:
+    int solve(int n, int x, int y, int z, vector<int> &dp)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(n < 0)
+        {
+            return INT_MIN;
+        }
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        int seg1 = 1 + solve(n - x, x, y, z, dp);
+        int seg2 = 1 + solve(n - y, x, y, z, dp);
+        int seg3 = 1 + solve(n - z, x, y, z, dp);
+        return dp[n] = max({seg1, seg2, seg3});
+    }
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        vector<int> dp(n + 1, -1);
+        int result = solve(n, x, y, z, dp);
+        return result < 0 ? 0 : result;
+    }
+}; 
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3037,3 +3037,21 @@ public:
         return true;
     }
 };
+// ********************************************************************************************Maximize Distance in Array(16/08)
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int smallest = arrays[0][0];
+        int biggest = arrays[0].back();
+        int max_distance = 0;
+
+        for (int i = 1; i < arrays.size(); ++i) {
+            max_distance = max(max_distance, abs(arrays[i].back() - smallest));
+            max_distance = max(max_distance, abs(biggest - arrays[i][0]));
+            smallest = min(smallest, arrays[i][0]);
+            biggest = max(biggest, arrays[i].back());
+        }
+
+        return max_distance;
+    }
+};
