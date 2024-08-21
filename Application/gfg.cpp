039b490a62ac1c8c6b2be40f1e03e245aebb5fb0
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-21 16:22:06 +0530

    21/08'

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3024,3 +3024,43 @@ int ans(Node*ptr , int &target , int &maxtime)
         return maxtime;
     }
 };
+// *******************************************************************************Shortest Path In Undirected Graph(21/08)
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        vector<int>dist(N, INT_MAX);
+        queue<int>q;
+        dist[src] = 0;
+        vector<vector<int>>adj(N);
+        for(auto it : edges)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        q.push(src);
+        int steps = 1;
+        while(!q.empty())
+        {
+            int sz = q.size();
+            while(sz--)
+            {
+                int node = q.front();
+                q.pop();
+                for(auto it : adj[node])
+                {
+                    if(dist[it] == INT_MAX)
+                    {
+                        dist[it] = steps;
+                        q.push(it);
+                    }
+                }
+            }
+            steps++;
+        }
+        for(int i = 0; i < N; i++)
+        {
+            if(dist[i] == INT_MAX) dist[i] = -1;
+        }
+        return dist;
+    }
+};
