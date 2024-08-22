be8ab38cfce7b2f548df1486deca647c7811f27d
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-22 16:27:27 +0530

    22/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3064,3 +3064,30 @@ class Solution {
         return dist;
     }
 };
+// **********************************************************************************Alien dictionary(22/08)
+string findOrder(string dict[], int N, int k)
+    {
+        vector<vector<int>> mat(k, vector<int>(k));
+        for (int i = 0; i + 1 < N; i++) {
+            string &a = dict[i], &b = dict[i + 1];
+            int n = a.size(), m = b.size(), j = 0;
+            while (j < n and j < m and a[j] == b[j])
+                j++;
+            if (j < n and j < m)
+                mat[a[j] - 'a'][b[j] - 'a'] = 1;
+        }
+        vector<int> vis(k);
+        string ans;
+        auto fun = [&](auto self, int u) -> void {
+            vis[u] = true;
+            for (int v = 0; v < k; v++)
+                if (mat[u][v] and !vis[v])
+                    self(self, v);
+            ans.push_back('a' + u);
+        };
+        for (int i = 0; i < k; i++)
+            if (!vis[i])
+                fun(fun, i);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3172,3 +3172,15 @@ public:
         return dp[i][j] = answer;
     }
 };
+// *******************************************************************************Number Compliment(22/08)
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        unsigned int mask = ~0;
+        while (num & mask) {
+            mask <<= 1;
+        }
+        return ~mask & ~num;
+    }
+};
