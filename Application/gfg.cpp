30252be462ebab2a86f7a80b52707912d5c4d8a2
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-23 17:41:00 +0530

    23/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3065,6 +3065,8 @@ class Solution {
     }
 };
 // **********************************************************************************Alien dictionary(22/08)
+class Solution(){
+public:
 string findOrder(string dict[], int N, int k)
     {
         vector<vector<int>> mat(k, vector<int>(k));
@@ -3091,3 +3093,30 @@ string findOrder(string dict[], int N, int k)
         reverse(ans.begin(), ans.end());
         return ans;
     }
+};
+// *************************************************************************************Lift View OF Binary Tree(23/08)
+vector<int> leftView(Node *root)
+{
+    vector<int> result;
+    if (root == NULL)
+        return result;
+    queue<pair<Node*, int>> q;
+    q.push({root, 0});
+    int max_level = -1;
+    while (!q.empty())
+    {
+        Node* node = q.front().first;
+        int level = q.front().second;
+        q.pop();
+        if (level > max_level)
+        {
+            result.push_back(node->data);
+            max_level = level;
+        }
+        if (node->left != NULL)
+            q.push({node->left, level + 1});
+        if (node->right != NULL)
+            q.push({node->right, level + 1});
+    }
+    return result;
+}
