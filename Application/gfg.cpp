3c1852f9f4da20b4ae3ed009154f19ba4e8fcede
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-08-24 20:08:53 +0530

    24/08

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3095,6 +3095,8 @@ string findOrder(string dict[], int N, int k)
     }
 };
 // *************************************************************************************Lift View OF Binary Tree(23/08)
+classs Solution(){
+    public:
 vector<int> leftView(Node *root)
 {
     vector<int> result;
@@ -3120,3 +3122,35 @@ vector<int> leftView(Node *root)
     }
     return result;
 }
+};
+// *******************************************************************************0 K-nap Sack Problem(24/08)
+class Solution {
+    public:
+  vector<vector<int>> memo;
+
+    int knapsackhelper(int index, int W, vector<int>& wt, vector<int>& val) {
+        if (index == 0 ) {
+            return 0;
+        }
+
+        if (memo[index][W] != -1) {
+            return memo[index][W];
+        }
+
+        int ans = 0;
+        if (wt[index - 1] <= W) {
+            ans += val[index - 1] + knapsackhelper(index - 1, W - wt[index - 1], wt, val);
+            } 
+            ans = max(ans,knapsackhelper(index - 1, W, wt, val));
+    
+
+        memo[index][W] = ans;
+        return ans;
+    }
+
+    int knapSack(int W, vector<int>& wt, vector<int>& val) {
+        int n = val.size();
+        memo.resize(n + 1, vector<int>(W + 1, -1));
+        return knapsackhelper(n, W, wt, val);
+    }
+};
