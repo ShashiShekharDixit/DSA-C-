1c19c87e8f6b510ded60d1bbc645218ae07ec26d
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-08 10:08:42 +0530

    08/09

--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3544,3 +3544,41 @@ public:
         return dfs(head, cur, root->left) || dfs(head, cur, root->right); 
     }
 };
+// *************************************************************************Split Linked List in Parts(08/09)
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ans(k);
+        int size = 0;
+        ListNode* current = head;
+        while (current != nullptr) {
+            size++;
+            current = current->next;
+        }
+        int splitSize = size / k;
+        int numRemainingParts = size % k;
+        current = head;
+        ListNode* prev = current;
+        for (int i = 0; i < k; i++) {
+            ListNode* newPart = current;
+            int currentSize = splitSize;
+            if (numRemainingParts > 0) {
+                numRemainingParts--;
+                currentSize++;
+            }
+            int j = 0;
+            while (j < currentSize) {
+                prev = current;
+                if (current != nullptr) {
+                    current = current->next;
+                }
+                j++;
+            }
+            if (prev != nullptr) {
+                prev->next = nullptr;
+            }
+            ans[i] = newPart;
+        }
+        return ans;
+    }
+};
