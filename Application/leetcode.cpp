9e1e37a98c212fdf533e9e5764d2419d6fd14dae
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-14 11:40:14 +0530

    14/09

--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3605,3 +3605,27 @@ public:
         return res;
     }
 };
+// ***********************************************************************************Longest Subarray With Maximum Bitwise AND(14/09)
+class Solution {
+public:
+    static int longestSubarray(vector<int>& nums) {
+        const int n=nums.size();
+        int m=max_element(nums.begin(), nums.end())-nums.begin(); //Find index of max element
+        int maxLen=0, x = nums[m];
+        for (int i = m; i < n; i++) {
+            int len = 0;
+            while (i < n && nums[i] == x) {
+                i++;
+                len++;
+            }
+            maxLen = max(maxLen, len);
+        }
+        return maxLen;
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
