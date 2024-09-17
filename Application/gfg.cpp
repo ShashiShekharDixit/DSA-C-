7036722c74fe59254eba8f167c4b0c6629dd249d
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-17 21:18:54 +0530

    17/09

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3562,3 +3562,19 @@ class Solution {
         return ans;
     }
 };
+// *******************************************************************************************Minimize the Height II(17/09)
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int min_diff = arr[n - 1] - arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] < k) continue;
+            int mini = min(arr[0] + k, arr[i] - k);
+            int maxi = max(arr[n - 1] - k, arr[i - 1] + k);
+            min_diff = min(min_diff, maxi - mini);
+        }
+        return min_diff;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3671,3 +3671,33 @@ public:
         return minDiff;
     }
 };
+// *************************************************************************************Uncommon Word From two Sentences(17/09)
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        string s = s1 + " " + s2;
+        cout<<s1.max_size();
+        unordered_map<string,int> um;
+        string tmp="";
+        for(auto& val : s){ 
+            if(val == ' '){
+                um[tmp]++;
+                tmp = "";
+            }
+            else{
+                tmp += val; 
+            }
+        }
+        if(tmp != ""){
+            um[tmp]++;
+            tmp = "";
+        }
+        vector<string> ans;
+        for(auto& val:um){ 
+            if(val.second==1){
+                ans.emplace_back(val.first);              
+            }
+        }
+        return ans;
+    }
+};
