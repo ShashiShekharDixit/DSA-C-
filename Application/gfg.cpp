1d3f47f20fb69e5962714a3a1894008c57d5e8a0
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-22 20:50:04 +0530

    22/09

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3578,3 +3578,25 @@ class Solution {
         return min_diff;
     }
 };
+// **********************************************************************************************Longest Prefix Suffix(22/09)
+class Solution {
+  public:
+    int lps(string str) {
+        int n = str.size();
+        vector<int> kmp(n);
+        int i = 0; 
+        for(int j = 1; j < n; j++){
+            if(str[i] == str[j]){
+                kmp[j] = ++i;
+            }
+            else {
+                while((--i) >= 0) {
+                    i = kmp[i];
+                    if(str[i] == str[j]) break;
+                }
+                kmp[j] = ++i;
+            }
+        }
+        return kmp[n - 1];
+    }
+};
