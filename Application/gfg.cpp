60d36f78ea9696ebfedb087036521c08c6027b8e
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-23 09:01:35 +0530

    23/09

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3600,3 +3600,27 @@ class Solution {
         return kmp[n - 1];
     }
 };
+// ****************************************************************************************Missing and Repeating(23/09)
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr)  {
+        vector<int> ans(2, -1);
+        int n = arr.size();
+        for(int i = 0; i < n; i++) {
+            int curr = abs(arr[i]);
+            if(arr[curr - 1] < 0) {
+                ans[0] = curr;
+            }
+            else {
+                arr[curr - 1] = -arr[curr - 1];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > 0) {
+                ans[1] = i + 1;
+                break;
+            }
+        }
+        return ans;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3729,3 +3729,60 @@ public:
         return num;
     }
 };
+// ********************************************************************************Extra Character in a string(23/09)
+const int N=26;
+struct Trie {
+    Trie* next[N];
+    bool isEnd=0;
+    Trie() {
+        fill(next, next+N, (Trie*)NULL);
+    }
+    ~Trie() {
+        for (int i=0; i<N; ++i) {
+            if (next[i] !=NULL) {
+                delete next[i];
+            }
+        }
+    }
+    void insert(string& word) {
+        Trie* Node=this;
+        for(char c: word){
+            int i=c-'a';
+            if(Node->next[i]==NULL)
+                Node->next[i]=new Trie();
+            Node=Node->next[i];
+        }
+        Node->isEnd=1;
+    }
+    void insert(vector<string>& dictionary){
+        for(auto& word: dictionary)
+            insert(word);
+    }
+};
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        Trie trie;
+        trie.insert(dictionary);
+
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        for (int i=n-1; i>=0; i--) {
+            dp[i]=dp[i+1]+1;
+            Trie* node=&trie;
+            for (int j=i; j<n; j++) {
+                if (node->next[s[j]-'a']==NULL) break;
+                node=node->next[s[j]-'a'];
+                if(node->isEnd) 
+                    dp[i]=min(dp[i], dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
