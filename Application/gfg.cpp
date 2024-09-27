9b4031c0188199d85478e48c1369710e7d0c2f08
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-27 18:45:48 +0530

    27/09

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3624,3 +3624,20 @@ class Solution {
         return ans;
     }
 };
+// ************************************************************************************************K sized subarray Maximum(27/09)
+class Solution {
+  public:
+    vector<int> max_of_subarrays(int k, vector<int> &arr) {
+        deque<int> dq;
+        vector<int> ans;
+        for(int i = 0; i < arr.size(); i++){
+            if(dq.size() && dq.front() == i - k) dq.pop_front();
+            while(dq.size() && arr[i] >= arr[dq.back()])dq.pop_back();
+            dq.push_back(i);
+            if(i >= k - 1){
+                ans.push_back(arr[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -3786,3 +3786,26 @@ auto init = []() {
     cout.tie(nullptr);
     return 'c';
 }();
+// **************************************************************************************My Calendar II(27/09)
+class MyCalendarTwo {
+    using int2 = pair<int, int>;
+    vector<int2> s1, s2;
+public:
+    MyCalendarTwo() {
+        s1.reserve(1000);
+        s2.reserve(1000);
+    }
+    bool book(int start, int end) {
+    for(auto& [s, t] : s2){
+        if(max(s, start)< min(t, end))
+        return 0;
+    }    
+    for(auto& [s, t]: s1){
+        int s0 = max(s, start), t0 = min(t, end);
+        if(s0 < t0)
+        s2.emplace_back(s0, t0);
+    }
+    s1.emplace_back(start, end);
+    return 1;
+    }
+};
