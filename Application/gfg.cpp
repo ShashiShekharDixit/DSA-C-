3b0356152953ae7f7e4f7c9916853c1584017950
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-30 10:33:36 +0530

    29/09

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3672,3 +3672,21 @@ class Solution {
         return cnt;
     }
 };
+// ***********************************************************************************Merge two BST(29/09)
+class Solution{
+public:
+     void inorder(Node *root, vector<int> &arr){
+        if(!root)
+        return;
+        inorder(root -> left, arr);
+        arr.push_back(root -> data);
+        inorder(root -> right, arr);
+    }
+    vector<int> merge(Node *root1, Node *root2) {
+        vector<int> arr;
+        inorder(root1, arr);
+        inorder(root2, arr);
+        sort(arr.begin(), arr.end());
+        return arr;
+    }
+}
