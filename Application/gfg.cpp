12227540d19993cc74559862a9e4d03de684beec
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-05 08:42:57 +0530

    Not a Subset Sum

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3801,3 +3801,15 @@ class Solution(){
         }
     }
 };
+// *************************************************************************************Not a Subset Sum(05/10)
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        int res = 1;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > res) return res;
+            res += arr[i];
+        }
+        return res;
+    }
+};
