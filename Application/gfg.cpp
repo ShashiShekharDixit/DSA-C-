a8bdcf270250f7f73ffbbec04c5a4d798f229a85
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-06 13:04:11 +0530

    06/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3813,3 +3813,34 @@ class Solution {
         return res;
     }
 };
+// ****************************************************************************************Find the NUmber of Islands(06/10)
+class Solution {
+public:
+    int dx[8]={0,1,1,1,0,-1,-1,-1};
+    int dy[8]={1,1,0,-1,-1,-1,0,1};
+    void dfs(std::vector<std::vector<char>>& grid, std::vector<std::vector<bool>>& visited,int i,int j,int n,int m){
+        visited[i][j] = true;
+        for (int k = 0; k < 8; k++) {
+            int nextidx_x = i + dx[k];
+            int nextidx_y = j + dy[k];
+            if (nextidx_x >= 0 && nextidx_x < n && nextidx_y >= 0 && nextidx_y < m && grid[nextidx_x][nextidx_y] == '1' && !visited[nextidx_x][nextidx_y]) {
+                dfs(grid, visited, nextidx_x, nextidx_y, n, m);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    dfs(grid,visited,i,j,n,m);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
