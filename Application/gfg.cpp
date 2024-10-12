def80562d0d3bc99401a9fcf42f2cea49698f92e
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-12 20:04:00 +0530

    12/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -3948,3 +3948,20 @@ class Solution {
         return v;     
     }
 };
+// *********************************************************************************Two samllest in every Subarray(12/10)
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int>& arr) {
+        int n = arr.size();
+        if(n < 2){
+            return -1;
+        }
+        int sum = arr[0] + arr[1];
+        int maxi = sum;
+        for(int i = 2; i < n; i++){
+            sum += (arr[i] - arr[i - 2]);
+            maxi = max(maxi, sum);
+        }
+        return maxi;
+    }
+};
