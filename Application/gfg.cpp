12137dd83a107a5197f809ad4522f50429cecc93
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-19 09:10:50 +0530

    Nearest Multiple Of 10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4078,3 +4078,23 @@ class Solution {
         return num;
     }
 };
+// **********************************************************************************************Nearest Multiple of 10(19/10)
+class Solution {
+  public:
+    string roundToNearest(string str) {
+        int n = str.size();
+        if( str[n - 1] <= '5') str[n - 1] = '0';
+        else {
+            str[n - 1] = '0';
+            int i = n - 2; bool flag = false;
+            while(i >= 0){
+                if(str[i] == '9') str[i] = '0';
+                else {
+                    str[i]++;
+                    flag = true;
+                    break;
+                } i--;
+            } if(!flag) str = '1' + str;
+        } return str;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -4330,3 +4330,15 @@ public:
         return count;
     }
 };
+// ************************************************************************************find the Kth bit in Nth Binary String(19/10)
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if(n == 1) return '0';
+        int length = (1 << n) - 1;
+        int mid = length / 2 + 1;
+        if(k == mid) return '1';
+        if(k < mid) return findKthBit(n - 1, k);
+        return findKthBit(n - 1, length - k + 1) == '0' ? '1' : '0';
+    }
+};
