fd9dd519dd796e75f3574651f0d74bab8e6d14e0
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-24 09:01:36 +0530

    24/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4122,3 +4122,29 @@ Class Solution {
         return head;
     }
 };
+// *******************************************************************************************Modify the Array(24/10)
+class Solution {
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) {
+        for(int i = 0; i < arr.size() - 1; i++)
+        {
+            if(arr[i] == arr[i + 1] && arr[i] != 0 && arr[ i + 1] != 0)
+            {
+                arr[i] = 2 * arr[i];
+                arr[i + 1] = 0;
+            }
+        }
+        int ind = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] != 0)
+            {
+                arr[ind++] = arr[i];
+            }
+        }
+        while(ind < arr.size())
+        {
+            arr[ind++] = 0;
+        }
+        return arr;
+    }
+};
--- Application/leetcode.cpp
+++ Application/leetcode.cpp
@@ -4376,3 +4376,21 @@ public:
         return st.top() == 't';
     }
 };
+// ****************************************************************************Flip Equivalent Binary Tree(24/10)
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(root1 == nullptr && root2 == nullptr) {
+            return true;
+        }
+        if(root1 == nullptr || root2 == nullptr){
+            return false;
+        }
+        if(root1 -> val != root2 -> val){
+            return false;
+        }
+        bool noSwap = flipEquiv(root1 -> left, root2 -> left) && flipEquiv(root1 -> right, root2 -> right);
+        bool swap = flipEquiv(root1 -> left, root2 -> right) && flipEquiv(root1 -> right, root2 -> left);
+        return swap || noSwap;
+    }
+};
