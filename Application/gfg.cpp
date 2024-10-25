1548b27bab00cc2f04da0340d21b8b2f5153f426
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-25 06:34:58 +0530

    25/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4148,3 +4148,22 @@ class Solution {
         return arr;
     }
 };
+// *************************************************************************************************Alternative Sorting(25/10)
+Class Solution {
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int i = 0;
+        int j = arr.size() - 1;
+        vector<int> ans;
+        while(i < j){
+            ans.push_back(arr[j]);
+            ans.push_back(arr[i]);
+            i++;
+            j--;
+        }
+        if(arr.size() % 2 != 0)
+        ans.push_back(arr[i]);
+        return ans;
+    }
+};
