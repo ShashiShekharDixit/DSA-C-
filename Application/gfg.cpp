9d071c382dbebe3e903b894a833a2535fa8f290f
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-26 09:34:06 +0530

    26/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4167,3 +4167,20 @@ Class Solution {
         return ans;
     }
 };
+// **********************************************************************************************Occurence of an Integer in a Linked List(26/10)
+class Solution {
+  public:
+    int count(struct Node* head, int key) {
+        Node* temp = head;
+        int cnt = 0;
+        while(temp != NULL)
+        {
+            if(temp -> data == key)
+            {
+                cnt++;
+            }
+            temp = temp -> next;
+        }
+        return cnt;
+    }
+};
