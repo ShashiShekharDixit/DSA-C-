bad72106ab362e2979f0b7f59f7ba014bbdc7dcc
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-28 23:07:12 +0530

    28/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4200,3 +4200,18 @@ class Solution {
         return 0;
     }
 };
+// *******************************************************************************************Remove Duplicate in Array(28/10)
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(auto it : arr){
+            if(mp.find(it) == mp.end()){
+                ans.push_back(it);
+                mp[it] = 1;
+            }
+        }
+        return ans;
+    }
+};
