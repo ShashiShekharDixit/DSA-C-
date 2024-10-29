9ab5b55651d9d65fd5781e7cd566a0cd1cd6ba1a
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-29 17:22:43 +0530

    29/10

--- Application/gfg.cpp
+++ Application/gfg.cpp
@@ -4215,3 +4215,28 @@ class Solution {
         return ans;
     }
 };
+// *********************************************************************************************Quick Osrt on Linked List(29/10)
+#include<algorithm>
+class Solution {
+  public:
+    struct Node* quickSort(struct Node* head) {
+        // code here
+        if(!head)
+        return head;
+        int i = 0;
+        vector<int> arr;
+        Node* temp = head;
+        while(temp != NULL){
+            arr.push_back(temp -> data);
+            temp = temp -> next;
+        }
+        sort(arr.begin(), arr.end());
+        temp = head;
+        while(temp != NULL){
+            temp -> data = arr[i];
+            i++;
+            temp = temp -> next;
+        }
+        return head;
+    }
+};
